cf15f25cf70e01e7411216c90cf1a5a204f25e7d
linux_china <libing.chen@gmail.com>
2020-05-31 18:00:53 -0700

    add hello for String

--- src/lib.rs
+++ src/lib.rs
@@ -6,5 +6,10 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
-  a + b
+    return a + b;
+}
+
+#[wasm_bindgen]
+pub fn hello(name: String) -> String {
+    return format!("hello {}", name);
 }
