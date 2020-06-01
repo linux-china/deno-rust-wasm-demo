c2c4a606fae0aac8febe3277840202513ae30e88
linux_china <libing.chen@gmail.com>
2020-05-31 18:01:17 -0700

    add hello

--- mod.ts
+++ mod.ts
@@ -1,10 +1,15 @@
 import init, {
-  source,
-  add as wasm_add,
+    source,
+    add as wasm_add,
+    hello as wasm_hello
 } from "./wasm.js";
 
 await init(source);
 
 export function add(a: number, b: number): number {
-  return wasm_add(a, b);
+    return wasm_add(a, b);
+}
+
+export function hello(name: string): string {
+    return wasm_hello(name);
 }
