f4b05c063adf13206694600b60db3c67d2960f81
linux_china <libing.chen@gmail.com>
2021-10-13 15:52:54 -0700

    Add fetch example

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-use wasm_bindgen::prelude::wasm_bindgen;
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Request, RequestInit, RequestMode, Response};
 
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
@@ -12,6 +16,32 @@ pub fn add(a: i32, b: i32) -> i32 {
 #[wasm_bindgen]
 pub fn hello(name: String) -> String {
     return format!("hello {}", name);
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Ip {
+    pub origin: String,
+}
+
+#[wasm_bindgen]
+pub async fn fetch_ip() -> Result<JsValue, JsValue> {
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+    opts.mode(RequestMode::Cors);
+    let url = format!("https://httpbin.org/ip");
+    let request = Request::new_with_str_and_init(&url, &opts)?;
+    let window = web_sys::window().unwrap();
+    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+    // `resp_value` is a `Response` object.
+    assert!(resp_value.is_instance_of::<Response>());
+    let resp: Response = resp_value.dyn_into().unwrap();
+
+    // Convert this other `Promise` into a rust `Future`.
+    let json = JsFuture::from(resp.json()?).await?;
+    // Use serde to parse the JSON into a struct.
+    let ip_info: Ip = json.into_serde().unwrap();
+    // Send the `IP` struct back to JS as an `Object`.
+    Ok(JsValue::from_serde(&ip_info).unwrap())
 }
 
 
