a8e733a81798160a4797cbef9195e9b7d24e9f79
linux_china <libing.chen@gmail.com>
2021-11-15 23:20:02 -0800

    Add helloBoa()

--- mod.ts
+++ mod.ts
@@ -2,6 +2,7 @@ import init, {
     source,
     add as wasm_add,
     hello as wasm_hello,
+    hello_boa,
     fetch_ip
 } from "./wasm.js";
 
@@ -13,6 +14,10 @@ export function add(a: number, b: number): number {
 
 export function hello(name: string): string {
     return wasm_hello(name);
+}
+
+export function helloBoa(name: string): string {
+    return hello_boa(name);
 }
 
 export function fetchIp(): {origin: string} {
