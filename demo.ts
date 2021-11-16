b3b333effa186e984a080ad0d112eb2dd25fba20
linux_china <libing.chen@gmail.com>
2021-11-15 23:20:16 -0800

    Add helloBoa

--- demo.ts
+++ demo.ts
@@ -1,6 +1,7 @@
-import {add, hello,fetchIp} from "./mod.ts";
+import {add, hello, fetchIp, helloBoa} from "./mod.ts";
 
 console.log(add(1, 2));
 console.log(hello("Jackie"))
+console.log(helloBoa("Jackie"))
 
 console.log(await fetchIp());
