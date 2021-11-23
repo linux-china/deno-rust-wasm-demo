3f6147db933020769c54a86ba437126bcabd4db4
linux_china <libing.chen@gmail.com>
2021-11-22 17:28:49 -0800

    Add addEventListener

--- demo.ts
+++ demo.ts
@@ -1,5 +1,9 @@
 import {add, hello, fetchIp, helloBoa} from "./mod.ts";
 
+addEventListener('demo', () => {
+    console.log('Demo event received!');
+});
+
 console.log(add(1, 2));
 console.log(hello("Jackie"))
 console.log(helloBoa("Jackie"))
